124d2d402f83a4b9900b883b055a72bc1c9f2b4b
voliseq <maciej.radziszowski@gmail.com>
2017-01-28 17:28:54 +0100

    init angular

---
+++ app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by voliseq on 28.01.2017.
+ */
+
+var stocksApp = angular.module('stocks', []);
---
+++ controllers/stocks-controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by voliseq on 28.01.2017.
+ */
+var stocksCtrl = stocksApp.controller('stocksCtrl', function(){
+    console.log("hello world");
+});
