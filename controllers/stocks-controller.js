8fa78dd0df6cc039bf65881050b318008f22a4e3
voliseq <maciej.radziszowski@gmail.com>
2017-01-29 16:12:25 +0100

    workin on context

--- controllers/stocks-controller.js
+++ controllers/stocks-controller.js
@@ -10,17 +10,23 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
             symbols.push(elem.Symbol);
         }
     });
-    var v = 800;
-    var h = 450;
+    var v = 960;
+    var h = 500;
     var margin = {
-        top: 58,
+        top: 20,
         bottom: 100,
-        left: 80,
-        right: 40
+        left: 40,
+        right: 20
+    },
+        margin2 = {
+        top: 430,
+        right: 20,
+        bottom: 30,
+        left: 40
     };
-
-    var width = v - margin.left - margin.right;
-    var height = h - margin.top - margin.bottom;
+    var width = v - margin.left - margin.right,
+        height = h - margin.top - margin.bottom,
+        height2 = h - margin2.top - margin2.bottom;
 
     var svg = d3.select("body").append("svg")
         .attr("id", "chart")
@@ -31,21 +37,35 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
         .classed("display", true)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var context = svg.append("g")
+        .classed("context", true)
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top +")");
+
     var dateParser = d3.timeParse("%Y-%m-%d");
     var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    var colorScale2 = d3.scaleOrdinal(d3.schemeCategory10);
     var x = d3.scaleTime()
         .domain(d3.extent(data, function (d) {
             return dateParser(d.Date);
         }))
         .range([0, width]);
-
+    var x2 = d3.scaleTime()
+        .domain(d3.extent(data, function (d) {
+            return dateParser(d.Date);
+        }))
+        .range([0, width]);
     var y = d3.scaleLinear()
         .domain([0, d3.max(data, function(d){
             return d.Close;
         })])
         .range([height, 0]);
-
+    var y2 = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d){
+            return d.Close;
+        })])
+        .range([height2, 0]);
     var xAxis = d3.axisBottom(x);
+    var xAxis2 = d3.axisBottom(x2);
     var yAxis = d3.axisLeft(y);
 
     var line = d3.line()
@@ -55,8 +75,29 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
         .y(function (d) {
             return y(d.Close)
     });
+    var line2 = d3.line()
+        .x(function (d) {
+            return x2(dateParser(d.Date));
+        })
+        .y(function (d) {
+            return y2(d.Close)
+        });
+    // var brush = d3.brushX()
+    //     .extent([[0, 0], [width, height2]])
+    //     .on("brush end", brushed);
+    //
+    // var zoom = d3.zoom()
+    //     .scaleExtent([1, Infinity])
+    //     .translateExtent([[0, 0], [width, height]])
+    //     .extent([[0, 0], [width, height]])
+    //     .on("zoom", zoomed);
 
-    //draw line
+    //draw
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
 
     //enter company
     chart.selectAll('.company')
@@ -67,8 +108,20 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
             return d;
         })
         .classed("company", true);
-
+    context.selectAll('.company')
+        .data(symbols)
+        .enter()
+        .append("g")
+        .attr("class", function(d){
+            return d;
+        })
+        .classed("company", true);
     //update company
+    context.selectAll(".company")
+        .style("fill", function(d,i){
+            return colorScale(i)
+        });
+    console.log(context.selectAll("path"));
     chart.selectAll(".company")
         .style("fill", function(d,i){
             return colorScale(i)
@@ -79,7 +132,6 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
         var arr = data.filter(function (elem) {
             return elem.Symbol == symbol;
         });
-
         //enter
         g.selectAll(".trendline")
             .data([arr])
@@ -117,9 +169,47 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
             .data(arr)
             .exit()
             .remove();
-
     });
+    symbols.forEach(function (symbol) {
+        var g = context.selectAll("g."+symbol);
+        var arr = data.filter(function (elem) {
+            return elem.Symbol == symbol;
+        });
+        //enter
+        g.selectAll(".trendline")
+            .data([arr])
+            .enter()
+            .append("path")
+            .classed("trendline", true);
 
+        g.selectAll(".point")
+            .data(arr)
+            .enter()
+            .append("circle")
+            .classed("point", true)
+            .attr("r", 2);
+        //update
+        g.selectAll(".trendline")
+            .attr("d", function(d){
+                return line2(d);
+            });
+        g.selectAll(".point")
+            .attr("cx", function (d) {
+                return x2(dateParser(d.Date));
+            })
+            .attr("cy", function (d) {
+                return y2(d.Close);
+            });
+        //exit
+        g.selectAll(".trendline")
+            .data([arr])
+            .exit()
+            .remove();
+        g.selectAll(".point")
+            .data(arr)
+            .exit()
+            .remove();
+    });
 
 
     // draw axes
@@ -134,6 +224,10 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$http', function ($http) {
         .attr("transform", "translate(0,0)")
         .call(yAxis);
 
+    context.append("g")
+        .classed("x axis", true)
+        .attr("transform", "translate(0, " + height2 +")")
+        .call(xAxis2);
     //
 
 
