1c734ccfdf31f3a221ae26601cbc465c09f144b1
voliseq <maciej.radziszowski@gmail.com>
2017-02-02 10:00:05 +0100

    clean up

--- controllers/stocks-controller.js
+++ controllers/stocks-controller.js
@@ -40,20 +40,4 @@ var stocksCtrl = stocksApp.controller('stocksCtrl', ['$scope', 'dataService', 'd
         $scope.stockData = dataService.betweenDates($scope.rawData, $scope.startDate, $scope.endDate);
     };
 
-    //
-    $scope.tabs = [
-        { title:'Dynamic Title 1', content:'Dynamic content 1' },
-        { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-    ];
-
-    $scope.alertMe = function() {
-        setTimeout(function() {
-            $window.alert('You\'ve selected the alert tab!');
-        });
-    };
-
-    $scope.model = {
-        name: 'Tabs'
-    };
-
 }]);
